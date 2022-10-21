f06838604674196b26d53a4176d803d7e84051d1
TGB85 <tammie.becker@planet.nl>
2022-10-21 11:14:49 +0200

    endpointtamara

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask
+import endpointtamara
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/helloworld2")
+def hello_world2():
+    return "<p>Hello, World nummer 2!</p>"
+
+@app.route("/helloworld3")
+def hello_world3():
+    return "<p>Hello, World nummer 3!</p>"
+
+@app.route("/endpointtamara")
+def functie2():
+    return endpointtamara.function_tamara()
