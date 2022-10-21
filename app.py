efa8994f313f1bc78bf53d68f9b894cc08595a05
erikfrima <erik.frima@gmail.com>
2022-10-21 11:24:22 +0200

    Commit van Erik

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from endpoint_erik import leuke_functie
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/erik")
+def endpoint_erik():
+    return leuke_functie
