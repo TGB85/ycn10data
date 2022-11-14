af7183446131fccbf875716c350b113643c041eb
TGB85 <tammie.becker@planet.nl>
2022-11-14 15:21:32 +0100

    json voor dorine

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import bestanderik
 #import endpointerik
 import endpointtamara
 from roelien import roelien
-import pandas as pd
 # import felixbestand
 #Voor nu uigecomment
 # import scrappen
@@ -18,14 +17,25 @@ app = Flask(__name__)
 def posters():
     return endpointtamara.three_posters()
 
+class Film:
+    title = 'hoi'
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, 
+            sort_keys=True, indent=4)
+
+@app.route("/tweede/<tekst>")
+def hello_world2(tekst):
+    mijnfilm = Film()
+    mijnfilm.title = tekst
+    mijnfilm.poster = 'https://m.media-amazon.com/images/M/MV5BOTY4ZmZjY2YtODg4ZS00YjlkLWJhOWMtMDU4Y2YyYjMwMDEzXkEyXkFqcGdeQXVyMTEzMTI1Mjk3._V1_SX300.jpg'
+    mijnfilm.plot = 'After the time of the Mane 6, Sunny--a young Earth Pony--and her new Unicorn friend Izzy explore their world and strive to restore Harmony to Equestria'
+    return mijnfilm.toJSON()
+
 @app.route("/dorine/<tekst>")
 def select_movies(tekst):
     film = {'tekst': tekst, 'title': 'My Little Pony: A New Generation', 'poster': 'https://m.media-amazon.com/images/M/MV5BOTY4ZmZjY2YtODg4ZS00YjlkLWJhOWMtMDU4Y2YyYjMwMDEzXkEyXkFqcGdeQXVyMTEzMTI1Mjk3._V1_SX300.jpg', 'plot': 'After the time of the Mane 6, Sunny--a young Earth Pony--and her new Unicorn friend Izzy explore their world and strive to restore Harmony to Equestria.'}
     result = json.dumps(film)
     return json.loads(result)
-
-
-# print(select_movies('tekst'))
 
 @app.route("/roelien")
 def test_roelien():
